577b3d616860a5b628a973ab521e771d342a8e50
JPandor <juwairiah1308@gmail.com>
2021-10-13 16:14:50 +0200

    finalized calculator
    Finished it and cleaned up js files

--- Javascript/calc.js
+++ Javascript/calc.js
@@ -1,19 +1,37 @@
-function writeToDisplayNumber (x) {
-    var num = document.getElementById("calcDisplay").innerHTML += x
-}
+let display = document.getElementById("calcDisplay").innerHTML
 
-function writeToDisplayOperator(a) {
-    var operand = document.getElementById("calcDisplay").innerHTML += a
+function writeToDisplay (x) {
+    display = document.getElementById("calcDisplay").innerHTML += x;
 }
 
 
 function result () {
-    let stringSum = document.getElementById("calcDisplay").innerHTML;
-    let sums = eval(stringSum)
+    let sums = eval(display);
     document.getElementById("calcDisplay").innerHTML = sums;
 }
 
+// function result () {
+//     display = document.getElementById("calcDisplay").innerHTML.split(' ');
+//     firstNumber = parseInt(display[0]);
+//     secondNumber = parseInt(display[2]);
+//     operator = display[1];
+//     if (operator == "+") {
+//         document.getElementById("calcDisplay").innerHTML = firstNumber + secondNumber;
+//     }
+//     else if (operator == "-"){
+//         document.getElementById("calcDisplay").innerHTML = firstNumber - secondNumber;
+//     }
+//     else if (operator == "*") {
+//         document.getElementById("calcDisplay").innerHTML = firstNumber * secondNumber;
+//     }
+//     else {
+//         document.getElementById("calcDisplay").innerHTML = firstNumber / secondNumber;
+//     }
+// }
 
+function reset() {
+    document.getElementById("calcDisplay").innerHTML = "";
+}
 
 
 
